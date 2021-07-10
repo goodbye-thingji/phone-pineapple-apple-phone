d26caa53b9c739aab5dd2555a49096834f6ed458
13circle <13circle97@gmail.com>
2021-07-10 12:55:13 +0900

    Initialize microservices - auth-user-service: user authentication & user-related features - board-service: board-oriented services - scraper-service: web scraping service to fetch & update data

---
+++ auth-user-service/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import logger from "morgan";
+
+dotenv.config();
+
+const { NODE_ENV, PORT } = process.env;
+
+const app = express();
+
+if (!NODE_ENV) {
+  throw Error("Error: NODE_ENV does not exist");
+}
+
+if (NODE_ENV === "production") {
+  app.use(logger("short"));
+} else if (NODE_ENV === "development") {
+  app.use(logger("dev"));
+} else {
+  throw Error("Error: Invalid NODE_ENV");
+}
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT || 8081, async () => {
+  if (NODE_ENV === "development") {
+    console.log(`Server listening on port ${PORT}`);
+  }
+});
